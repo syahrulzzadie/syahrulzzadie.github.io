8b46b5010e788b7271f3f45fc0fc80899b8dee1a
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2018-12-10 10:29:29 +0700

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -3,6 +3,8 @@ function getElementId(idElement){
 }
 
 window.onload = () => {
+  var title = getElementId('myTitle');
   var body = getElementId('myapp');
+  title.innerHTML = "Syahrul Adiguna Kurniawan";
   body.innerHTML = "Hai";
 };
